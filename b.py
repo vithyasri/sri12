8d3ddc432964929c6a496293c30add6211640f1c
vithyasri <52270841+vithyasri@users.noreply.github.com>
2019-07-17 12:06:18 +0530

    Create b.py

---
+++ b.py
@@ -0,0 +1,16 @@
+String1 = "GeeksForGeeks"
+print("Initial String: ") 
+print(String1) 
+
+print("\nFirst character of String is: ") 
+print(String1[0]) 
+
+print("\nLast character of String is: ") 
+print(String1[-1]) 
+
+print("\nSlicing characters from 3-12: ") 
+print(String1[3:12]) 
+
+print("\nSlicing characters between " +
+    "3rd and 2nd last character: ") 
+print(String1[3:-2]) 
